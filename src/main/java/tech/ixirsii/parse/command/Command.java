a857c3aec5104e6bbb507413dd0abde471548346
Ryan Porterfield <ixirsii@ixirsii.tech>
2025-07-21 11:39:02 -0700

    chore(zed): Remove Zed project settings

--- src/main/java/tech/ixirsii/parse/command/Command.java
+++ src/main/java/tech/ixirsii/parse/command/Command.java
@@ -1,13 +1,5 @@
 package tech.ixirsii.parse.command;
 
-import lombok.NonNull;
-import lombok.extern.slf4j.Slf4j;
-import tech.ixirsii.parse.internal.ArgumentEvent;
-import tech.ixirsii.parse.internal.ArgumentMatch;
-import tech.ixirsii.parse.internal.ArgumentPair;
-import tech.ixirsii.parse.internal.InternalEvent;
-import tech.ixirsii.parse.parser.Parser;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -16,6 +8,13 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.SequencedMap;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import tech.ixirsii.parse.internal.ArgumentEvent;
+import tech.ixirsii.parse.internal.ArgumentMatch;
+import tech.ixirsii.parse.internal.ArgumentPair;
+import tech.ixirsii.parse.internal.InternalEvent;
+import tech.ixirsii.parse.parser.Parser;
 
 /**
  * Command class.
@@ -25,6 +24,7 @@ import java.util.SequencedMap;
  */
 @Slf4j
 public final class Command {
+
     /**
      * GNU prefix.
      */
@@ -39,26 +39,31 @@ public final class Command {
      */
     @NonNull
     private final String about;
+
     /**
      * GNU long options.
      */
     @NonNull
     private final SequencedMap<String, OptionalArgument<?>> longOptions;
+
     /**
      * Command name.
      */
     @NonNull
     private final String name;
+
     /**
      * Required (positional) arguments.
      */
     @NonNull
     private final List<PositionalArgument<?>> positionalArguments;
+
     /**
      * POSIX short options.
      */
     @NonNull
     private final SequencedMap<Character, OptionalArgument<?>> shortOptions;
+
     /**
      * Usage message.
      */
@@ -75,20 +80,24 @@ public final class Command {
      * @param positionalArguments Required (positional) arguments.
      */
     public Command(
-            @NonNull final String name,
-            @NonNull final String about,
-            @NonNull final String usage,
-            @NonNull final Collection<OptionalArgument<?>> optionalArguments,
-            @NonNull final Collection<PositionalArgument<?>> positionalArguments) {
+        @NonNull final String name,
+        @NonNull final String about,
+        @NonNull final String usage,
+        @NonNull final Collection<OptionalArgument<?>> optionalArguments,
+        @NonNull final Collection<PositionalArgument<?>> positionalArguments
+    ) {
         final OptionalArgument<Boolean> help = new OptionalArgument<>(
-                "help",
-                'h',
-                "Show this help message",
-                Parser.BOOLEAN_PARSER);
-        final SequencedMap<String, OptionalArgument<?>> longOptionMap =
-                LinkedHashMap.newLinkedHashMap(optionalArguments.size() + 1);
-        final SequencedMap<Character, OptionalArgument<?>> shortOptionMap =
-                LinkedHashMap.newLinkedHashMap(optionalArguments.size() + 1);
+            "help",
+            'h',
+            "Show this help message",
+            Parser.BOOLEAN_PARSER
+        );
+        final SequencedMap<String, OptionalArgument<?>> longOptionMap = LinkedHashMap.newLinkedHashMap(
+            optionalArguments.size() + 1
+        );
+        final SequencedMap<Character, OptionalArgument<?>> shortOptionMap = LinkedHashMap.newLinkedHashMap(
+            optionalArguments.size() + 1
+        );
 
         longOptionMap.put(help.getName(), help);
         shortOptionMap.put(help.getShortOption(), help);
@@ -106,48 +115,47 @@ public final class Command {
         this.usage = usage;
     }
 
-    public CommandResult parse2(@NonNull final List<String> arguments) {
+    /**
+     * Parse command input that has already been split, such as JVM command line arguments.
+     *
+     * @param arguments Split input.
+     * @return Command result.
+     */
+    public CommandResult parse(@NonNull final List<String> arguments) {
         log.trace("Parsing arguments {}", arguments);
 
         final InternalEvent.InternalEventBuilder builder = InternalEvent.builder();
-        final Map<Integer, List<Argument<?>>> matchedArguments = HashMap.newHashMap(arguments.size());
+        final Map<Integer, List<Argument<?>>> matchMap = HashMap.newHashMap(arguments.size());
 
         for (int i = 0; i < arguments.size(); ++i) {
             final String argument = arguments.get(i);
-            final List<ArgumentMatch> argumentMatches = getArgument(argument);
+            final List<ArgumentMatch> matchedArguments = getArguments(argument);
 
-            if (argumentMatches.size() == 1) {
-                final ArgumentMatch argumentMatch = argumentMatches.getFirst();
+            if (!matchedArguments.isEmpty()) {
+                final List<Argument<?>> recognizedArguments = new ArrayList<>(matchedArguments.size());
 
-                if (argumentMatch.match() == null) {
-                    builder.unrecognized(argument);
-                } else {
-                    matchedArguments.put(i, Collections.singletonList(argumentMatch.match()));
-                }
-            } else if (!argumentMatches.isEmpty()) {
-                final List<Argument<?>> matchedShortOptions = new ArrayList<>(argumentMatches.size());
-
-                for (final ArgumentMatch argumentMatch : argumentMatches) {
+                for (final ArgumentMatch argumentMatch : matchedArguments) {
                     if (argumentMatch.match() == null) {
                         builder.unrecognized(argumentMatch.argument());
                     } else {
-                        matchedShortOptions.add(argumentMatch.match());
+                        recognizedArguments.add(argumentMatch.match());
                     }
                 }
 
-                matchedArguments.put(i, matchedShortOptions);
+                matchMap.put(i, recognizedArguments);
             }
         }
 
-        int j = 0;
-        for (int i = 0; i < arguments.size(); ++i) {
+        for (int i = 0, j = 0; i < arguments.size(); ++i) {
             final String argument = arguments.get(i);
 
-            if (matchedArguments.containsKey(i)) {
-                final Argument<?> argument = matchedArguments.get(i);
+            if (matchMap.containsKey(i)) {
+                final List<Argument<?>> matchedArguments = matchMap.get(i);
 
-                if (argument.getValueCount() == ArgumentValueCount.ONE) {
-
+                for (final Argument<?> matchedArgument : matchedArguments) {
+                    if (matchedArgument.getValueCount() == ArgumentValueCount.ZERO) {
+                        builder.event(matchedArgument.getName(), matchedArgument.parse(argument, ""));
+                    }
                 }
             } else if (j < positionalArguments.size()) {
                 final PositionalArgument<?> positionalArgument = positionalArguments.get(j);
@@ -160,156 +168,28 @@ public final class Command {
         }
     }
 
-    /**
-     * Parse input that has already been split, such as JVM command line arguments.
-     *
-     * @param arguments Split input.
-     * @return New internal.
-     */
-    public CommandResult parse(@NonNull final List<String> arguments) {
-        log.trace("Parsing arguments {}", arguments);
-
-        final InternalEvent.InternalEventBuilder builder = InternalEvent.builder();
-        final List<ArgumentPair> argumentsWithValues = new ArrayList<>(arguments.size());
-        final List<PositionalArgument<?>> unmatchedArguments = new ArrayList<>(positionalArguments);
-
-        // Build the argument map, mapping argument indices to arguments.
-        for (int i = 0; i < arguments.size(); ++i) {
-            // TODO: Split argument
-            final String argument = arguments.get(i);
-            final List<ArgumentMatch> argumentMatches = getArgument(argument);
-
-            if (argumentMatches.isEmpty()) {
-                if (!unmatchedArguments.isEmpty()) {
-                    final Argument<?> positionalArgument = unmatchedArguments.removeFirst();
-                    argumentsWithValues.add(new ArgumentPair(positionalArgument, argument));
-                } else {
-                    builder.unrecognized(argument);
-                }
-            } else if (argumentMatches.size() == 1) {
-                final ArgumentMatch argumentMatch = argumentMatches.getFirst();
-
-                if (argumentMatch.match() != null) {
-                    final Argument<?> mappedArgument = argumentMatch.match();
-
-                    if (mappedArgument.getValueCount() == ArgumentValueCount.ZERO || i >= arguments.size() - 1) {
-                        argumentsWithValues.add(new ArgumentPair(mappedArgument, ""));
-                    } else if (mappedArgument.getValueCount() == ArgumentValueCount.ZERO_OR_ONE) {
-                        // TODO: this
-                    } else {
-                        // TODO: Split argument
-                        final String nextArgument = arguments.get(i + 1);
-                        final List<ArgumentMatch> nextArgumentMatches = getArgument(nextArgument);
-
-                        if (nextArgumentMatches.size() == 1) {
-                            final ArgumentMatch nextArgumentMatch = nextArgumentMatches.getFirst();
-
-                            if (nextArgumentMatch.match() == null) {
-                                // The next argument is actually the value for this argument, skip processing
-                                ++i;
-                                argumentsWithValues.add(new ArgumentPair(mappedArgument, nextArgument));
-                            } else {
-                                argumentsWithValues.add(new ArgumentPair(mappedArgument, ""));
-                            }
-                        }
-                    }
-                }
-            } else {
-                for (final ArgumentMatch argumentMatch : argumentMatches) {
-                    if (argumentMatch.match() == null) {
-                        builder.unrecognized(argumentMatch.argument());
-                    } else {
-                        argumentsWithValues.add(new ArgumentPair(argumentMatch.match(), ""));
-                    }
-                }
-            }
-        }
-
-        final InternalEvent internalEvent = builder.build();
-
-        log.debug("Parsed command internal {}", internalEvent);
-
-        if (isValid(internalEvent)) {
-            return new CommandResult(internalEvent.toCommandEvent(), true, "");
-        } else {
-            return new CommandResult(internalEvent.toCommandEvent(), false, getErrorMessage(internalEvent));
-        }
-    }
-
     /* ******************************************** Override methods ******************************************** */
-
-    /**
-     * Convert event to string.
-     *
-     * @param commandEvent Internal command event to convert.
-     * @return String representation of the event.
-     */
-    private String getErrorMessage(@NonNull final InternalEvent commandEvent) {
-        final StringBuilder builder = new StringBuilder("{")
-                .append(System.lineSeparator());
-        final Map<String, ArgumentEvent<?>> events = commandEvent.events();
-
-        for (final Map.Entry<String, ArgumentEvent<?>> event : events.entrySet()) {
-            final ArgumentEvent<?> value = event.getValue();
-
-            builder.append("    ");
-
-            if (!value.isSuccess()) {
-                builder.append(value.name())
-                        .append("=\"")
-                        .append(value.value())
-                        .append("\": ")
-                        .append(value.errorMessage())
-                        .append(System.lineSeparator());
-            } else {
-                builder.append(value.name())
-                        .append("=\"")
-                        .append(value.value())
-                        .append("\"")
-                        .append(System.lineSeparator());
-            }
-        }
-
-        for (final PositionalArgument<?> positionalArgument : positionalArguments) {
-            if (!events.containsKey(positionalArgument.getName())) {
-                builder.append("    ")
-                        .append(positionalArgument)
-                        .append(" is missing but is required")
-                        .append(System.lineSeparator());
-            }
-        }
-
-        if (!commandEvent.unrecognized().isEmpty()) {
-            builder.append("    Unrecognized arguments: ")
-                    .append(commandEvent.unrecognized())
-                    .append(System.lineSeparator());
-        }
-
-        return builder.append("}").toString();
-    }
 
     @Override
     public String toString() {
         final StringBuilder stringBuilder = new StringBuilder(name)
-                .append(System.lineSeparator())
-                .append(about)
-                .append(System.lineSeparator())
-                .append(System.lineSeparator())
-                .append("Usage:")
-                .append(System.lineSeparator())
-                .append(usage)
-                .append(System.lineSeparator())
-                .append(System.lineSeparator())
-                .append("Options:")
-                .append(System.lineSeparator());
+            .append(System.lineSeparator())
+            .append(about)
+            .append(System.lineSeparator())
+            .append(System.lineSeparator())
+            .append("Usage:")
+            .append(System.lineSeparator())
+            .append(usage)
+            .append(System.lineSeparator())
+            .append(System.lineSeparator())
+            .append("Options:")
+            .append(System.lineSeparator());
 
         for (final OptionalArgument<?> option : longOptions.sequencedValues()) {
             stringBuilder.append(option).append(System.lineSeparator());
         }
 
-        stringBuilder.append(System.lineSeparator())
-                .append("Arguments:")
-                .append(System.lineSeparator());
+        stringBuilder.append(System.lineSeparator()).append("Arguments:").append(System.lineSeparator());
 
         for (final PositionalArgument<?> argument : positionalArguments) {
             stringBuilder.append(argument).append(System.lineSeparator());
@@ -320,35 +200,85 @@ public final class Command {
 
     /* **************************************** Private utility methods ***************************************** */
 
-    private List<ArgumentMatch> getArgument(@NonNull final String argument) {
+    private List<ArgumentMatch> getArguments(@NonNull final String argument) {
         if (argument.equals(GNU_PREFIX) || argument.equals(POSIX_PREFIX)) {
             return Collections.singletonList(new ArgumentMatch(argument, null));
         } else if (argument.startsWith(GNU_PREFIX)) {
             final String longOption = argument.substring(GNU_PREFIX.length());
-            final OptionalArgument<?> optionalArgument = longOptions.get(longOption);
 
-            return Collections.singletonList(new ArgumentMatch(argument, optionalArgument));
+            return Collections.singletonList(new ArgumentMatch(argument, longOptions.get(longOption)));
         } else if (argument.startsWith(POSIX_PREFIX)) {
             final String flags = argument.substring(POSIX_PREFIX.length());
-            final List<ArgumentMatch> matchedShortOptions = new ArrayList<>(flags.length());
+            final List<ArgumentMatch> arguments = new ArrayList<>(flags.length());
 
             for (final char flag : flags.toCharArray()) {
-                final OptionalArgument<?> optionalArgument = shortOptions.get(flag);
-
-                matchedShortOptions.add(new ArgumentMatch("-" + flag, optionalArgument));
+                arguments.add(new ArgumentMatch("-" + flag, shortOptions.get(flag)));
             }
 
-            return matchedShortOptions;
+            return Collections.unmodifiableList(arguments);
         } else {
             return Collections.emptyList();
         }
     }
 
     /**
-     * Check if the internal is valid.
+     * Convert event to string.
      *
-     * @param commandEvent Command internal to check.
-     * @return {@code true} if the internal is valid, otherwise {@code false}.
+     * @param commandEvent Internal command event to convert.
+     * @return String representation of the event.
+     */
+    private String getErrorMessage(@NonNull final InternalEvent commandEvent) {
+        final StringBuilder builder = new StringBuilder("{").append(System.lineSeparator());
+        final Map<String, ArgumentEvent<?>> events = commandEvent.events();
+
+        for (final Map.Entry<String, ArgumentEvent<?>> event : events.entrySet()) {
+            final ArgumentEvent<?> value = event.getValue();
+
+            builder.append("    ");
+
+            if (!value.isSuccess()) {
+                builder
+                    .append(value.name())
+                    .append("=\"")
+                    .append(value.value())
+                    .append("\": ")
+                    .append(value.errorMessage())
+                    .append(System.lineSeparator());
+            } else {
+                builder
+                    .append(value.name())
+                    .append("=\"")
+                    .append(value.value())
+                    .append("\"")
+                    .append(System.lineSeparator());
+            }
+        }
+
+        for (final PositionalArgument<?> positionalArgument : positionalArguments) {
+            if (!events.containsKey(positionalArgument.getName())) {
+                builder
+                    .append("    ")
+                    .append(positionalArgument)
+                    .append(" is missing but is required")
+                    .append(System.lineSeparator());
+            }
+        }
+
+        if (!commandEvent.unrecognized().isEmpty()) {
+            builder
+                .append("    Unrecognized arguments: ")
+                .append(commandEvent.unrecognized())
+                .append(System.lineSeparator());
+        }
+
+        return builder.append("}").toString();
+    }
+
+    /**
+     * Check if the command is valid.
+     *
+     * @param commandEvent Command command to check.
+     * @return {@code true} if the command is valid, otherwise {@code false}.
      */
     private boolean isValid(@NonNull final InternalEvent commandEvent) {
         log.trace("Validating command internal");
